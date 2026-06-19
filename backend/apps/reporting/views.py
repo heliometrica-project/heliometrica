732c969d241689a98fd1f1f0c7c323b104d0260e
Jean-Richardson <jeanrichardson88@gmail.com>
2026-06-19 18:20:17 -0300

    feat(#19): implementar exportacao CSV com ReportService e endpoint

--- backend/apps/reporting/views.py
+++ backend/apps/reporting/views.py
@@ -1,7 +1,9 @@
 from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
 
 from apps.reporting.models import GenerationHistory
 from apps.reporting.serializers import GenerationHistorySerializer
+from apps.reporting.services import ReportService
 
 
 class GenerationHistoryViewSet(viewsets.ModelViewSet):
@@ -13,3 +15,18 @@ class GenerationHistoryViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['get'], url_path='export')
+    def export_csv(self, request, pk=None):
+        history = self.get_object()
+        service = ReportService()
+        response, filename = service.generate_csv_response(
+            estimate=history.estimate,
+            title=history.title,
+        )
+        service.register_export(
+            user=request.user,
+            estimate=history.estimate,
+            filename=filename,
+        )
+        return response
